69c9b84249f52d123bcc67dbe761fc83994dfd72
PrinceXtremeX <princextreme429@gmail.com>
2025-07-24 08:14:00 -0400

    Update kickall-slow.js

--- plugins/kickall-slow.js
+++ plugins/kickall-slow.js
@@ -6,7 +6,7 @@ cmd({
     pattern: "kickall1",
     alias: ["kickall", "endgc", "endgroup"],
     desc: "Remove all non-admin members from the group.",
-    react: "🎉",
+    react: "🧨",
     category: "group",
     filename: __filename,
 }, 
@@ -14,14 +14,14 @@ async (conn, mek, m, {
     from, groupMetadata, groupAdmins, isBotAdmins, senderNumber, reply, isGroup, isOwner, isAdmins
 }) => {
     try {
-        if (!isGroup) return reply("This command can only be used in groups.");
+        if (!isGroup) return reply("*_ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ɪɴ ɢʀᴏᴜᴘs_*");
 
         if (!isOwner && !isAdmins) {
-            return reply("Only the bot owner or group admins can use this command.");
+            return reply("*_ᴏɴʟʏ ᴛʜᴇ ʙᴏᴛ ᴏᴡɴᴇʀ ᴏʀ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ_*");
         }
 
         if (!isBotAdmins) {
-            return reply("I need to be an admin to execute this command.");
+            return reply("*_ɪ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴀɴ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ_*");
         }
 
         const allParticipants = groupMetadata.participants;
@@ -31,7 +31,7 @@ async (conn, mek, m, {
             return reply("There are no non-admin members to remove.");
         }
 
-        reply(`Starting to remove ${nonAdminParticipants.length} non-admin members...`);
+        reply(`*sᴛᴀʀᴛɪɴɢ ᴛᴏ ʀᴇᴍᴏᴠᴇ ${nonAdminParticipants.length} ɴᴏɴ-ᴀᴅᴍɪɴ ᴍᴇᴍʙᴇʀs*`);
 
         for (let participant of nonAdminParticipants) {
             try {
@@ -42,7 +42,7 @@ async (conn, mek, m, {
             }
         }
 
-        reply("Successfully removed all non-admin members from the group.");
+        reply("*_sᴜᴄᴄᴇssғᴜʟʟʏ ʀᴇᴍᴏᴠᴇᴅ ᴀʟʟ ɴᴏɴ-ᴀᴅᴍɪɴ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ_*");
     } catch (e) {
         console.error("Error removing non-admin users:", e);
         reply("An error occurred while trying to remove non-admin members. Please try again.");
