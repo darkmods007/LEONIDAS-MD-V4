6df4fc3ec712f0ab829ee8709ffcc5976549b85f
PrinceXtremeX <princextreme429@gmail.com>
2025-07-26 01:44:20 -0400

    Add files via upload

---
+++ plugins/about.js
@@ -0,0 +1,40 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    alias: "bot",
+    react: "🤖",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `╭─ 「 *\`MINI BOT ONLINE\`* 」
+│꙳ *ʙᴏᴛ ɴᴀᴍᴇ* ↔ ᴍɪɴɪ xᴛʀᴇᴍᴇ
+│꙳ *sᴛᴀᴛᴜs* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ᴀᴜᴛᴏ ʀᴇsᴛᴀʀᴛ* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ʙᴏᴛ ʀᴜɴ* ↔${uptime}
+│꙳ *ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ*
+╰────────────────❍
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`
+await conn.sendMessage(from, {
+    image: { url: 'https://files.catbox.moe/p01dj2.jpg' },
+    caption: about,
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363418161689316@newsletter', // ou ton JID actuel
+            newsletterName: '𝐌𝐈𝐍𝐈-𝐗𝐓𝐑𝐄𝐌𝐄',
+            serverMessageId: 143
+        }
+    }
+}, { quoted: mek })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
